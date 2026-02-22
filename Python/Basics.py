da35ed7a24f9f01e4d9638c4c55d6b92a5915214
Malahim Ansari <malahimtech@gmail.com>
2026-02-23 04:34:13 +0500

    Python Basic

---
+++ Python/Basics.py
@@ -0,0 +1,11 @@
+
+print("Hello World")
+
+# Taking Input in Python
+name = input("Enter your name: ")
+print("Hello " + name)
+
+# Taking Integer Input
+age = int(input("Enter your age: "))
+print("Your age is: " + str(age))
+
