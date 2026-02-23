ff18e01a19d45a5a60d677bd751f2208e56e2e6c
Malahim Ansari <malahimtech@gmail.com>
2026-02-23 05:43:25 +0500

    Data Types

---
+++ Python/DataTypes.py
@@ -0,0 +1,45 @@
+
+# Data Types in Python
+# Numeric: int, float, complex
+# Sequence Type: string, list, tuple
+# Mapping Type: dict
+# Boolean: bool
+# Set Type: set, frozenset
+# Binary Types: bytes, bytearray, memoryview
+
+
+# Numeric Data Types:
+
+a = 10  # int
+b = 3.14  # float
+c = 3 + 4j  # complex
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'float'>    
+print(type(c))  # <class 'complex'>
+
+# Sequence Data Types:
+s = "Hello, World!"  # string
+l = [1, 2, 3, 4, 5]  # list
+t = (1, 2, 3, 4, 5)  # tuple
+print(type(s))  # <class 'str'>
+print(type(l))  # <class 'list'>
+print(type(t))  # <class 'tuple'>
+
+
+# Boolean Data Type:
+x = True
+y = False
+print(type(x))  # <class 'bool'>
+
+
+
+# Set Data Types:
+s1 = {1, 2, 3}  # set
+s2 = frozenset([4, 5, 6])  # frozenset
+print(type(s1))  # <class 'set'>
+print(type(s2))  # <class 'frozenset'>
+
+# Dictonary Data Type:
+d = {'name': 'Malahim', 'age': 20}  # dict
+print(type(d))  # <class 'dict'>
+
